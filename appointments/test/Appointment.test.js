01104be9168814c0a176a336da5fd15b72cfc82f
Gerald <codyhenry527@gmail.com>
2023-12-12 10:38:39 -0500

    updated test

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,8 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Appointment } from "../src/Appointment";
+
 /* For React testing, give the describe block the same name as
 the component itself, or the function name */
 describe("Appointment", () => {
   //define a test and a description
   it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    ReactDOM.createRoot(container).render(component);
     expect(document.body.textContent).toContain("Ashley");
   });
 });
