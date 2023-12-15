cfc69d102ded1696c25dc5b4e2dc22f39a4ca65b
Gerald <codyhenry527@gmail.com>
2023-12-15 10:59:49 -0500

    init form component and tests

---
+++ appointments/test/CustomerForm.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  initializeReactContainer,
+  render,
+  element,
+  form,
+} from "./reactTestExtensions";
+
+import { CustomerForm } from "../src/CustomerForm";
+
+describe("CustomerForm", () => {
+  beforeEach(() => {
+    initializeReactContainer();
+  });
+
+  it("renders a form", () => {
+    render(<CustomerForm />);
+    expect(form()).not.toBeNull();
+  });
+});
